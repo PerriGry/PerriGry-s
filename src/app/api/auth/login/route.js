341c6bfe49e672f1e1626e856e67289a13e5033a
Cristian Valderrama <valderramacabezasc@gmail.com>
2025-09-06 20:23:10 -0500

    create servicesDir & split authDir

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,16 +1,6 @@
 import pool from "@/lib/db";
 
-// GET: listar usuarios
-export async function GET() {
-  try {
-    const result = await pool.query("SELECT * FROM usuarios");
-    return new Response(JSON.stringify(result.rows), { status: 200 });
-  } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), { status: 500 });
-  }
-}
-
-// POST: crear usuario
+// LOGIN
 export async function POST(req) {
   try {
     const body = await req.json();
