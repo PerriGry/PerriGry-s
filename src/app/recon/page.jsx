4465171200e200c0cd5618eca9b7bdf4b18a8ad5
Cristian Valderrama <valderramacabezasc@gmail.com>
2025-09-17 19:17:18 -0500

    Change Main Color To Black

--- src/app/recon/page.jsx
+++ src/app/recon/page.jsx
@@ -3,7 +3,7 @@ import { Recognition } from "@/components/index";
 
 export default function Home() {
   return (
-    <main className="p-6">
+    <main className="p-6 bg-black min-h-screen text-white">
       <h1 className="text-2xl font-bold mb-4">
         👤 Reconocimiento Facial en Cliente
       </h1>
